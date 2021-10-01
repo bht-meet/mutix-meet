9c3d372c14f2fbf7953eb7668def71eb116fd81d
nanocosmos - Oliver Lietz <lietz@nanocosmos.de>
2021-10-01 19:44:50 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,9 @@ export default function Index() {
   return (
     <div>
       <Head>
-        <title>Global Meet By Mutix</title>
+        <title>Global Meet</title>
         <link rel="icon" href="/favicon.ico" />
-        <script src="//jitsi.jothon.online/libs/lib-jitsi-meet.min.js" />
+        <script src="//meeting.bht-media.de/libs/lib-jitsi-meet.min.js" />
         <script
           src="//code.jquery.com/jquery-3.5.1.min.js"
           integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
