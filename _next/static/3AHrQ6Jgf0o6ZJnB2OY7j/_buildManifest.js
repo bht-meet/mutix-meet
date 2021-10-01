6e787076e975c879cb51f863a9cb5325cb991c28
nanocosmos-ol <lietz@nanocosmos.de>
2021-10-01 20:04:41 +0000

    Deploying to gh-pages from  @ f2f709f697ed9015e12e46833e9493577f5593e3 🚀

---
+++ _next/static/3AHrQ6Jgf0o6ZJnB2OY7j/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-5f355f85910146c1cdf2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f26d0039f18efbe52100.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
